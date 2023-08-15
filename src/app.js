891cc35d52527f691d63781c18935946c039680c
FrancescaAmeche <121627825+FrancescaAmeche@users.noreply.github.com>
2023-08-15 14:40:05 +0000

    domain name generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our", "my"];
+  let adj = ["colorful", "magical", "shining"];
+  let noun = ["rainbow", "unicorn", "dragon"];
+
+  let createDomainNames = (arr1 = [], arr2 = [], arr3 = []) => {
+    let domainNames = [];
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j < arr2.length; j++) {
+        for (let k = 0; k < arr3.length; k++) {
+          domainNames.push(arr1[i] + arr2[j] + arr3[k] + ".com");
+        }
+      }
+    }
+    return domainNames;
+  };
+  console.log(createDomainNames(pronoun, adj, noun));
 };
